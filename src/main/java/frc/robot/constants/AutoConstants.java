c8fad815ae06fb33fb32b11b3a7b166bdd23bb57
J.A. Shue <j.a.shue@outlook.com>
2020-01-18 09:31:50 -0800

    deploy correct pathfile

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -2,9 +2,9 @@ package frc.robot.constants;
 
 public class AutoConstants {
 
-	public static final double kMaxAccelerationMetersPerSecondSquared = 0;
-	public static final double kMaxSpeedMetersPerSecond = 0;
-	public static final double kRamseteB = 0;
-	public static final double kRamseteZeta = 0;
+	public static final double kMaxAccelerationMetersPerSecondSquared = 4;
+	public static final double kMaxSpeedMetersPerSecond = 4;
+	public static final double kRamseteB = 0.8;
+	public static final double kRamseteZeta = 2;
 
 }

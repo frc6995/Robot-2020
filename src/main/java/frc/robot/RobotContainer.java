8418cade8a2dc8131c7fabe1b9ff5b7c7c5d8b63
sammcdo <sam_mcdo@yahoo.com>
2020-01-25 07:20:16 -0800

    more good changes - Sam

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,17 +8,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import static edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import frc.robot.Constants.CONTROLLER_TYPE;
-import frc.robot.commands.BasicAutoCG;
-import frc.robot.subsystems.DrivebaseS;
-import frc.robot.subsystems.SparkMaxS;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.BasicAutoCG;
+import frc.robot.subsystems.DrivebaseS;
+import frc.robot.subsystems.SparkMaxS;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a

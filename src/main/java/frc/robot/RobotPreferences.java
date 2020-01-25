058cc2c577faea08a07b93e288f4b84dc823d111
sammcdo <sam_mcdo@yahoo.com>
2020-01-25 06:56:42 -0800

    changed name - Sam

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -8,5 +8,5 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Preferences {
+public final class RobotPreferences {
 }

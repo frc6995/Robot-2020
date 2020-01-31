a3257e6e47258889cc677cf1f4f9311d8c2d4bc2
AriShivkopanazak <ari.shivkopanazak@gmail.com>
2020-01-31 11:31:50 -0800

    ari's test branch for the entirety of the robot bu

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands.Drivebase"},{"l":"frc.robot.subsystems"},{"l":"frc.wrappers.MotorControllers"},{"l":"frc.wrappers.Preferences"}]
-=======
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands"},{"l":"frc.robot.subsystems"},{"l":"frc.utility.preferences"},{"l":"frc.wrappers.MotorControllers"}]
->>>>>>> sammcdo/5/climberS
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands.Drivebase"},{"l":"frc.robot.subsystems"},{"l":"frc.utility.preferences"},{"l":"frc.wrappers.MotorControllers"}]

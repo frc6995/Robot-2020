22007c4fd5d0628e7c38b8064fa0b7d360d88de2
J.A. Shue <j.a.shue@outlook.com>
2020-02-08 15:13:44 -0800

    Added ShooterConstants

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.constants;
+
+/**
+ * Add your docs here.
+ */
+public class ShooterConstants {
+    /**
+     * The target RPM for the shooter wheel.
+     */
+    public static final double SHOOTER_RPM = 6000;
+    /**
+     * The CAN ID of the SPARK MAX running the shooter NEO.
+     */
+    public static final int CAN_ID_SHOOTER_SPARK_MAX = 32;
+}

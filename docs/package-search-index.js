e512cfcbaf763642669d3caaf6aa58fcf7fa1991
J.A. Shue <j.a.shue@outlook.com>
2020-02-10 18:41:40 -0800

    added docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands"},{"l":"frc.robot.subsystems"},{"l":"frc.utility.preferences"},{"l":"frc.wrappers.MotorControllers"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands.auto"},{"l":"frc.robot.commands.drivebase"},{"l":"frc.robot.constants"},{"l":"frc.robot.subsystems"},{"l":"frc.utility"},{"l":"frc.utility.preferences"},{"l":"frc.wrappers.MotorControllers"}]

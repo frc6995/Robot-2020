2bc702bfb900b122d04a5c53fe401d17020c9d46
Ari <ari.shashivkopanazak@gmail.com>
2020-02-13 09:24:13 -0800

    opting to go with shujas vision

--- src/main/java/frc/robot/commands/Drivebase/DrivebaseVisonC.java
+++ src/main/java/frc/robot/commands/Drivebase/DrivebaseVisonC.java
@@ -14,6 +14,7 @@ import frc.robot.RobotContainer;
  * in order to Aim the shooter at the top power port
  * 
  * @author Ari Shashivkopanazak
+ * @deprecated opting for a wpilib PID manager
  */
 public class DrivebaseVisonC extends CommandBase {
 

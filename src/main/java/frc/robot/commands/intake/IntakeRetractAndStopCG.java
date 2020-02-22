246dac630670d4813d59f0ef79efe29d8e9511c4
J.A. Shue <j.a.shue@outlook.com>
2020-02-22 11:40:45 -0800

    intake system working in sim

---
+++ src/main/java/frc/robot/commands/intake/IntakeRetractAndStopCG.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.IntakeS;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class IntakeRetractAndStopCG extends SequentialCommandGroup {
+  /**
+   * Creates a new IntakeRetractAndStopCG.
+   */
+  public IntakeRetractAndStopCG(IntakeS intake) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(new InstantCommand(() -> intake.intakeRetract(), intake),
+    new InstantCommand(() -> intake.intakeMotor(0.0), intake));
+  }
+}

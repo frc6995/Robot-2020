9277808bf2cc8484d1fe16b6dbd3fb548b73e0a6
sammcdo <sam_mcdo@yahoo.com>
2020-02-23 13:03:11 -0800

    updated preferences to values from climber testing - Sam

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -14,22 +14,22 @@ import frc.utility.preferences.NomadIntPreference;
  */
 public final class RobotPreferences {
     //Climber Constants
-    public static final NomadDoublePreference liftHeight = new NomadDoublePreference("liftHeight", 7500);
+    public static final NomadDoublePreference liftHeight = new NomadDoublePreference("liftHeight", 66500);
     public static final NomadDoublePreference pullHeight = new NomadDoublePreference("pullHeight", 3000);
     
-    public static final NomadDoublePreference climberKpUp = new NomadDoublePreference("Climber Kp Up", 0.0);
-    public static final NomadDoublePreference climberKiUp = new NomadDoublePreference("Climber Ki Up", 0.0);
-    public static final NomadDoublePreference climberKdUp = new NomadDoublePreference("Climber Kd Up", 0.0);
+    public static final NomadDoublePreference climberKpUp = new NomadDoublePreference("Climber Kp Up", 0.5);
+    public static final NomadDoublePreference climberKiUp = new NomadDoublePreference("Climber Ki Up", 0.0001);
+    public static final NomadDoublePreference climberKdUp = new NomadDoublePreference("Climber Kd Up", 10);
     public static final NomadDoublePreference climberKf = new NomadDoublePreference("Climber Kf", 0.0);
 
-    public static final NomadDoublePreference climberKpDown = new NomadDoublePreference("Climber Kp Up", 0.0);
-    public static final NomadDoublePreference climberKiDown = new NomadDoublePreference("Climber Ki Up", 0.0);
-    public static final NomadDoublePreference climberKdDown = new NomadDoublePreference("Climber Kd Up", 0.0);
+    public static final NomadDoublePreference climberKpDown = new NomadDoublePreference("Climber Kp Up", 0.06);
+    public static final NomadDoublePreference climberKiDown = new NomadDoublePreference("Climber Ki Up", 0.0001);
+    public static final NomadDoublePreference climberKdDown = new NomadDoublePreference("Climber Kd Up", 8);
 
-    public static final NomadIntPreference climberIZoneUp = new NomadIntPreference("Climber I zone up", 300);
-    public static final NomadIntPreference climberIZoneDown = new NomadIntPreference("Climber I zone down", 300);
+    public static final NomadIntPreference climberIZoneUp = new NomadIntPreference("Climber I zone up", 1000);
+    public static final NomadIntPreference climberIZoneDown = new NomadIntPreference("Climber I zone down", 1000);
 
-    public static final NomadIntPreference climberAllowableError = new NomadIntPreference("Climber Allowable Error", 15);
+    public static final NomadIntPreference climberAllowableError = new NomadIntPreference("Climber Allowable Error", 1000);
 
     
     public static NomadBooleanPreference hopperInvert = new NomadBooleanPreference("hopper motors invert", true);
